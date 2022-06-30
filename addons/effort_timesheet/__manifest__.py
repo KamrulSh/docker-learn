a47ae84ac238c586f41f44cbf849b5ec76caa0c2
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2022-06-30 11:08:01 +0600

    updated custom module

--- addons/effort_timesheet/__manifest__.py
+++ addons/effort_timesheet/__manifest__.py
@@ -2,11 +2,11 @@
     'name': 'Employee Effort Timesheet',
     'sequence': 200,
     'version': '1.0',
-    'summary': 'Employee Effort calculations in timesheet',
+    'summary': 'Employee Salary calculations encrypted',
     'author': 'Kamrul and Niazi',
     'category': 'Human Resources',
     'description': 'This is an application where employee can save daily work effort and calculate total effort in a timesheet.',
-    'depends': ['base', 'hr', 'project','hr_holidays_public'],
+    'depends': ['base', 'hr'],
     'data': [
         'security/ir.model.access.csv',
         'views/effort_timesheet.xml',
--- addons/effort_timesheet/models/effort_timesheet.py
+++ addons/effort_timesheet/models/effort_timesheet.py
@@ -1,21 +1,80 @@
-from datetime import datetime, time
-from email.policy import default
+import base64
 
-from dateutil.rrule import rrule, DAILY
 from odoo import models, fields, api
-from pytz import UTC
 
 
 class EffortTimesheet(models.Model):
     _name = 'effort.timesheet'
     _description = 'Here effort timesheet information is stored.'
 
-    encrypted = fields.Encrypted()
+    employee_id = fields.Many2one('hr.employee', string="Employee")
 
-    duration = fields.Float("Duration", encrypt='encrypted', default=10)
-    price_unit = fields.Float(encrypt='encrypted', default=22)
-    price_unit2 = fields.Float(encrypt='encrypted', default=1334)
-    price_unit3 = fields.Integer(encrypt='encrypted', default=1434543)
-    price_unit4 = fields.Float(encrypt='encrypted', default=13334)
-    price_unit5 = fields.Boolean(encrypt='encrypted', default=True)
-    my_selection = fields.Selection([('option1', 'Label 1'), ('option2', 'Label 2')], string='My Selection Field', encrypt='encrypted')
+    # salary = fields.Char("Salary", editable=True, compute='_compute_salary')
+    # provident_fund = fields.Char("Provident fund", editable=True, compute='_compute_pf')
+
+    salary = fields.Char("Salary", store=True, compute='_compute_salary', inverse='_decrypt_salary')
+    provident_fund = fields.Char("Provident fund", store=True, compute='_compute_pf', inverse='_decrypt_pf')
+
+    def encrypt(self, message):
+        print(f"Encrypting: {message}")
+        message_bytes = message.encode('ascii')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('ascii')
+        # base64_message = str(message) + "0000000000000000"
+        print(f"Encrypted: {base64_message}")
+        return base64_message
+
+    def decrypt(self, base64_message):
+        print(f"Decrypting: {base64_message}")
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        message = message_bytes.decode('ascii')
+        # message = base64_message[::-1]
+        print(f"Decrypted: {message}")
+        return message
+
+    @api.depends('salary')
+    def _compute_salary(self):
+        print("----- entered_salary")
+        # base64_sa = self.decrypt(self.salary)
+        # print(f"Enc Salary string: {base64_sa}")
+        # self.salary = str(base64_sa)
+        # self.write({'salary': base64_sa})
+
+        for rec in self:
+            base64_sa = self.decrypt(rec.salary) if rec.salary else None
+            print(f"Enc Salary string: {base64_sa}")
+            rec.salary = str(base64_sa)
+
+    @api.depends('provident_fund')
+    def _compute_pf(self):
+        print("----- entered_prov_fund")
+        # base64_pf = self.decrypt(self.provident_fund)
+        # print(f"Enc PF string: {base64_pf}")
+        # self.provident_fund = str(base64_pf)
+        # self.write({'provident_fund': base64_pf})
+
+        for rec in self:
+            base64_pf = self.decrypt(rec.provident_fund) if rec.provident_fund else None
+            print(f"Enc PF string: {base64_pf}")
+            rec.provident_fund = str(base64_pf)
+
+    def _decrypt_salary(self):
+        print("+++++ entered_salary 1")
+        # pass
+        for rec in self:
+            salary_string = self.encrypt(rec.salary) if rec.salary else None
+            print(f"Dec salary string: {salary_string}")
+            rec.salary = salary_string
+
+            base64_sa = self.decrypt(rec.salary) if rec.salary else None
+            print(f"Enc Salary string: {base64_sa}")
+            rec.salary = str(base64_sa)
+
+    def _decrypt_pf(self):
+        print("+++++ entered_prov_fund 1")
+        # pass
+        for rec in self:
+            pf_string = self.encrypt(rec.provident_fund) if rec.provident_fund else None
+            print(f"Dec PF string: {pf_string}")
+            rec.provident_fund = pf_string
